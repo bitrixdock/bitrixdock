a89e3008283e1a1f3c7d8816bfa1ce2e78deadd9
M_A_X_NSK <hardstyle@myttk.ru>
2022-02-11 11:54:33 +0700

    Фикс скриптов

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 set -e
 
 # This script is meant for quick & easy install via:
-#   $ curl -fsSL https://raw.githubusercontent.com/bitrixdock/bitrixdock/master/install.sh -o install.sh | sh install.sh
-
+# $ curl -fsSL https://raw.githubusercontent.com/bitrixdock/bitrixdock/master/install.sh -o install.sh | sh install.sh
 echo "Check requirments"
+apt-get -qq update
 hash git 2>/dev/null || { apt-get install -y git; }
 hash docker 2>/dev/null || { cd /usr/local/src && wget -qO- https://get.docker.com/ | sh; }
-hash docker-compose 2>/dev/null || { curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && source ~/.bashrc; }
+hash docker-compose 2>/dev/null || { curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose; }
 
 echo "Create folder struct"
 mkdir -p /var/www/bitrix && \
 cd /var/www/bitrix && \
-wget http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php && \
+wget https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php && \
 cd /var/www/ && \
 git clone https://github.com/bitrixdock/bitrixdock.git && \
 cd /var/ && chmod -R 775 www/ && chown -R root:www-data www/ && \
