5310943813aece2f802692021129811679bd1167
vitams <vmpartner@gmail.com>
2023-05-27 00:37:33 +0500

    WSL

--- install.wsl.sh
+++ install.wsl.sh
@@ -3,17 +3,16 @@ set -e
 
 echo "Create folder struct"
 mkdir -p /var/www/bitrix && \
-cd /var/www/bitrix && \
+cd /var/www && \
 rm -f /var/www/bitrix/bitrixsetup.php && \
-wget https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php && \
-cd /var/www/ && \
+wget https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php -P /var/www/bitrix/ && \
 rm -rf /var/www/bitrixdock && \
 git clone https://github.com/bitrixdock/bitrixdock.git && \
-cd /var/ && chmod -R 775 www/ && chown -R root:www-data www/ && \
-cd /var/www/bitrixdock
+chmod -R 775 /var/www/bitrix && chown -R root:www-data /var/www/bitrix && \
 
 echo "Config"
-cp -f .env_template .env
+cp -f /var/www/bitrix/.env_template /var/www/bitrix/.env
 
 echo "Run"
-docker compose -p bitrixdock up -d
+docker compose -p bitrixdock down
+docker compose -f /var/www/bitrix/docker-compose.yml -p bitrixdock up -d
