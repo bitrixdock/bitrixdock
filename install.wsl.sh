682e75d29fd85ed2326187d6292cba6d78b149d2
vitams <vmpartner@gmail.com>
2023-05-27 00:19:36 +0500

    WSL

--- install.wsl.sh
+++ install.wsl.sh
@@ -4,8 +4,10 @@ set -e
 echo "Create folder struct"
 mkdir -p /var/www/bitrix && \
 cd /var/www/bitrix && \
+rm -f /var/www/bitrix/bitrixsetup.php && \
 wget https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php && \
 cd /var/www/ && \
+rm -rf /var/www/bitrixdock && \
 git clone https://github.com/bitrixdock/bitrixdock.git && \
 cd /var/ && chmod -R 775 www/ && chown -R root:www-data www/ && \
 cd /var/www/bitrixdock
