2f46e840168167e49ab339c451c8e35ff1a4294e
Dmitry Verkhoturov <paskal.07@gmail.com>
2024-01-24 21:20:06 +0100

    switch all wget usages to curl
    Both are used currently, but keeping both doesn't make sense.
    
    Automatic installation is recommended via curl, so curl is
    supposed to be installed on the user's machine.

--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ set -e
 echo "Check requirements"
 apt-get -qq update
 hash git 2>/dev/null || { apt-get install -y git; }
-hash docker 2>/dev/null || { cd /usr/local/src && wget -qO- https://get.docker.com/ | sh; }
+hash docker 2>/dev/null || { cd /usr/local/src && curl -fsSL https://get.docker.com/ | sh; }
 hash docker-compose 2>/dev/null || { curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose; }
 
 echo "Create folder struct"
 mkdir -p /var/www/bitrix && \
-cd /var/www/bitrix && \
-wget https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php && \
+curl -fsSL https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php -o /var/www/bitrix/bitrixsetup.php && \
 cd /var/www/ && \
 git clone https://github.com/bitrixdock/bitrixdock.git && \
 cd /var/ && chmod -R 775 www/ && chown -R root:www-data www/ && \
