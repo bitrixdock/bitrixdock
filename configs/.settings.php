b9ead9dca37c96c0fdf05b8751386239abab6359
Maksim Cherkasov <33313896+Elnadrion@users.noreply.github.com>
2024-11-10 12:32:53 +0100

    Небольшие правки (#234)

--- configs/.settings.php
+++ configs/.settings.php
@@ -1,68 +1,71 @@
 <?php
 
 return [
-    'utf_mode' =>
-        [
-            'value' => true,
-            'readonly' => true,
+    'utf_mode' => [
+        'value' => true,
+        'readonly' => true,
+    ],
+    'cache_flags' => [
+        'value' => [
+            'config_options' => 3600,
+            'site_domain' => 3600,
         ],
-    'cache_flags' =>
-        [
-            'value' =>
-                [
-                    'config_options' => 3600,
-                    'site_domain' => 3600,
+        'readonly' => false,
+    ],
+    'session' => [
+        'value' => [
+            'mode' => 'default',
+            'handlers' => [
+                'general' => [
+                    'type' => 'memcache',
+                    'keyPrefix' => '#01',
+                    'port' => 11211,
+                    'host' => 'memcached',
                 ],
-            'readonly' => false,
+            ],
         ],
+    ],
     'cache' => [
         'value' => [
             'type' => 'memcache',
             'sid' => 'master',
             'memcache' => [
                 'host' => 'memcached',
-                'port' => '11211'
+                'port' => '11211',
             ],
         ],
         'readonly' => true,
     ],
-    'cookies' =>
-        [
-            'value' =>
-                [
-                    'secure' => false,
-                    'http_only' => true,
-                ],
-            'readonly' => false,
+    'cookies' => [
+        'value' => [
+            'secure' => false,
+            'http_only' => true,
         ],
-    'exception_handling' =>
-        [
-            'value' =>
-                [
-                    'debug' => true,
-                    'handled_errors_types' => 4437,
-                    'exception_errors_types' => 4437,
-                    'ignore_silence' => false,
-                    'assertion_throws_exception' => true,
-                    'assertion_error_type' => 256,
-                    'log' => null,
-                ],
-            'readonly' => false,
+        'readonly' => false,
+    ],
+    'exception_handling' => [
+        'value' => [
+            'debug' => true,
+            'handled_errors_types' => 4437,
+            'exception_errors_types' => 4437,
+            'ignore_silence' => false,
+            'assertion_throws_exception' => true,
+            'assertion_error_type' => 256,
+            'log' => null,
         ],
-    'connections' =>
-        [
-            'value' =>
-                [
-                    'default' =>
-                        [
-                            'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
-                            'host' => 'db',
-                            'database' => 'bitrix',
-                            'login' => 'root',
-                            'password' => '123',
-                            'options' => 2,
-                        ],
-                ],
-            'readonly' => true,
+        'readonly' => false,
+    ],
+    'connections' => [
+        'value' => [
+            'default' => [
+                'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+                'host' => 'db',
+                'database' => 'bitrix',
+                'login' => 'bitrix',
+                'password' => '123',
+                'options' => 2.0,
+            ],
         ],
+        'readonly' => true,
+    ],
 ];
