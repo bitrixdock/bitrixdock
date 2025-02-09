8da172758786b127c4a064b9d2fc4a955fe45243
sergei.smirnov <sergei.smirnov@poleinvest.ru>
2025-02-09 13:46:05 +0300

    feat: fix comment

--- mkcert/entrypoint.sh
+++ mkcert/entrypoint.sh
@@ -4,5 +4,6 @@ CERT_KEY="/etc/nginx/certs/bitrix.loc-key.pem"
 CERT_FILE="/etc/nginx/certs/bitrix.loc-cert.pem"
 
 if [ ! -f "$CERT_KEY" ] || [ ! -f "$CERT_FILE" ]; then
+    mkcert -install
     mkcert -key-file "$CERT_KEY" -cert-file "$CERT_FILE" bitrix.loc
 fi
